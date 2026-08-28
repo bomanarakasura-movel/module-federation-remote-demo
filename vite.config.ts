ff440786168c90a56e6a0f480c15337f9d58358c
bomsiwor <bomsiwor@gmail.com>
2026-08-28 14:57:09 +0700

    fix: remove base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { federation } from '@module-federation/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/module-federation-remote-demo/",
+  // base: "/module-federation-remote-demo/",
   plugins: [
     react(),
     federation({
